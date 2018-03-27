87d8901e7a57d93cfa8f7fa204ac616dcb6f2513
Soomti <soomti@likelion.org>
2018-03-27 21:21:17 +0900

    facebook error

--- README.md
+++ README.md
@@ -2,4 +2,11 @@
 
 #### 데일리 큐티 WEB SERVICE Version
 
+- www.dailyquiettime.com
+
+### 이 서비스를 통해
+
+- ssl https 설정을 할줄 알게 되었습니다
+- log 를 더 잘 볼수 있게 되었습니다.흐흐...ㅜㅜㅜㅜ
+
 *made by soomti*
--- db/migrate/20180319083104_devise_create_users.rb
+++ db/migrate/20180319083104_devise_create_users.rb
@@ -4,7 +4,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-
+      t.string :profile_image
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
